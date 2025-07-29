a01967345690e71374195fe20a85fa71346bc4c9
Antonio Araujo <tonioara1994@gmail.com>
2025-07-29 18:17:27 +1200

    CreaTuPrimeraLanding+Araujo

--- verduleria-app/src/componentes/Carrito.js
+++ verduleria-app/src/componentes/Carrito.js
@@ -1,23 +1,19 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import { X } from 'lucide-react'; 
 import '../Estilos/Carrito.css'; 
 
-function Carrito({ onClose }) {
-  const [isOpen,setIsOpen]=useState(false)
-
-  const toggleIsOpen = () => {
-    setIsOpen(!isOpen)
-  }
+function Carrito({ isOpen, onClose }) {
+  
+  if (!isOpen) return null;
 
   return (
     <>
-      <div className="carrito-lateral-simple__overlay" onClick={toggleIsOpen}></div>
 
       <div className="carrito-lateral-simple">
         <div className="carrito-lateral-simple__encabezado">
           <h2 className="carrito-lateral-simple__titulo">Tu Carrito</h2>
-          <button onClick={toggleIsOpen} className="carrito-lateral-simple__boton-cerrar">
+          <button onClick={onClose} className="carrito-lateral-simple__boton-cerrar">
             <X size={24} /> 
           </button>
         </div>
